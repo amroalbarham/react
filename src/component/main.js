a127f44e64daf44bd20989f17c251fdf5c6a8458
amroalbarham <amroalbarham@gmail.com>
2021-05-17 06:01:16 +0300

    some edit

--- src/component/main.js
+++ src/component/main.js
@@ -7,19 +7,13 @@ class Main extends React.Component{
         return(
             <div>
               
-              <HornedBeast title={'green ball'}  />
+              <HornedBeast title={'UniWhal'} imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg' } alt={'roger '}   descreption={'A unicorn and a narwhal nuzzling their horns'}  />
              
-             <HornedBeast  imageUrl={'https://stillmed.olympics.com/media/Images/OlympicOrg/News/2017/05/03/tennis-thumbnail.jpg?interpolation=lanczos-none&resize=300:300' } alt={'roger '} />
-             <h4>
-             <HornedBeast descreption={'tennis'} />
-           </h4> 
-           <h2>
-              <HornedBeast title={'tennis love'}  />
-             </h2> 
-            <HornedBeast  imageUrl={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFUcCxdct5L2nFJTJmbKKJgyrLkNV2ufnvJw&usqp=CAU' } alt={'roger'}/>
-             <h4>
-             <HornedBeast descreption={'tennis'} />
-           </h4> 
+         
+           
+              <HornedBeast title={'Rhino Family'}  imageUrl={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80' } alt={'roger'} descreption={'Mother (or father) rhino with two babies'} />
+             
+          
            </div> 
         )
     }
