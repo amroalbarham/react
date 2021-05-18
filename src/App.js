56fb43d3b164ee8ccc48a84114e5b285b01009dd
amroalbarham <amroalbarham@gmail.com>
2021-05-18 13:03:04 +0300

    adf

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Header from './component/header';
 import Footer from './component/footer';
 import Main from './component/main';
+import Data from './component/data.json';
+
 
 
 
 
 class App extends React.Component {
+
+  
   render() {
     return (
       <div>
       <Header/>
-      <Main/>
+      <Main
+      Data={Data}
+      />
       <Footer/>
        </div>
     )
--- src/component/main.js
+++ src/component/main.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import Data from './data.json'
-import {CardColumns} from 'react-bootstrap'
+// import Data from './data.json'
+import { CardColumns } from 'react-bootstrap'
 
 
 class Main extends React.Component {
-    
+
 
     render() {
         return (
             <CardColumns>
-            <div className='new'>
-         
-                {Data.map(item => {
-                    return (
-                        <HornedBeast
-                            title={item.title}
-                            imageUrl={item.image_url}
-                            descreption={item.description}
-                        />
-                    ) })
-                }
+                <div className='new'>
+
+                    {this.props.Data.map(item => {
+                        return (
+                            <HornedBeast
+                                title={item.title}
+                                imageUrl={item.image_url}
+                                descreption={item.description}
+                            />
+                        )
+                    })
+                    }
 
 
 
-            </div>
+                </div>
             </CardColumns>
         )
     }
